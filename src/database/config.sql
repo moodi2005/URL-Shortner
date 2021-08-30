fc0bb15d627d954c8086f1d5bca23e02a9b76741
amir-najafi-1 <amir.najafi.contact@gmail.com>
2021-08-30 11:36:49 +0430

    edit config.psql

--- src/database/config.sql
+++ src/database/config.sql
@@ -1,10 +1,10 @@
-/* CREATE DATABASE urlshortener; */
+-- CREATE DATABASE urlshortener;
 CREATE TABLE urlshortened (
-    	ShortenedLink CHAR (4) UNIQUE,
+ShortenedLink CHAR (4) UNIQUE,
 	NotShortenedLink TEXT,
 	NumberOfClick SMALLINT,
 	ShowNumberOfClickLink CHAR (6) UNIQUE,
 	IP TEXT,
     	UserID VARCHAR (4),
 	CreateTime TIMESTAMPTZ DEFAULT NOW()
-);
+)
